bb00020adae808806d8936fd84761a03174639ed
shiva <shivarajput.pp98@gmail.com>
2021-03-08 18:21:09 +0530

    added database credentials

--- partials/conn.php
+++ partials/conn.php
@@ -1,8 +1,8 @@
 <?php
-$server = "localhost";
-$user_name = "root";
-$password = "";
-$db = "quora";
+$server = "freedb.tech";
+$user_name = "freedbtech_shiva";
+$password = "shiva0525";
+$db = "freedbtech_quora";
 
 $conn = mysqli_connect($server,$user_name,$password,$db);
 if(!$conn){
